5bd5c208cad01a72a1071210c5a5a4cae0ddda22
Fernanda Olivares <folivareslisperguer@gmail.com>
2024-04-07 22:31:34 -0400

    intento numero 2

--- index2.js
+++ index2.js
@@ -1,3 +1,18 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    function laCajaDePandora(numero) {
+        if (numero % 2 === 0) {
+        return numero.toString(2);
+        } else {
+        return numero.toString(16);
+        }
+        }
+        
+        function personalInfo() {
+        return {
+        name: "Fernanda Olivares",
+        age: "35",
+        nationality: "Chilean",
+        city: "Santiago"
+        };
+        }
     }
