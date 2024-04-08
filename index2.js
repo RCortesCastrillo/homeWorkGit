ad1375c5f56a74b27ecef96cdb95b5b8af198837
Victor Sepulveda <victorseva123@gmail.com>
2024-04-07 21:31:20 -0500

    Prueba 4

--- index2.js
+++ index2.js
@@ -1,11 +1,4 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
-    "Henry 2024"
-
-    }
-
- function presentacion(){
-    "Victor Alfonso Sepulveda"
-    "38 años"
-    "Colombiano"
- }   
+    "Victor Sepulveda"
+}
